1e6aaaeffe3d53a9f5f148335d108e757dd172ed
imkewlhuh <rafael.lopez0827@gmail.com>
2023-03-09 17:57:14 -0500

    prod testing

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,10 +2,8 @@ let apiURL = '';
 
 if (process.env.NODE_ENV === 'development') {
     apiURL = 'http://localhost:8080';
-    console.log(apiURL);
 } else {
-    apiURL = import.meta.env.API_URL;
-    console.log(apiURL);
+    apiURL = "https://petapi-production.up.railway.app";
 }
 
 export const baseURL = apiURL;
