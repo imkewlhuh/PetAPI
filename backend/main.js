cab4bdab9849a4ef997b5e596614f0326c9d8c7f
imkewlhuh <rafael.lopez0827@gmail.com>
2023-03-08 23:15:04 -0500

    prod testing

--- backend/main.js
+++ backend/main.js
@@ -3,5 +3,5 @@ import createServer from "./server.js";
 const server = createServer();
 
 server.listen(8080, () => {
-    console.log("App is listening at http://localhost:8080");
+    console.log("App is listening at https://petapi-production.up.railway.app");
 });
