f07ba2da559bc5da23998f344f56d9a0a9269541
imkewlhuh <rafael.lopez0827@gmail.com>
2023-03-09 14:14:48 -0500

    prod testing

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,1 +1,9 @@
-export const baseURL = import.meta.env.API_URL || "http://localhost:8080";
+let apiURL = '';
+
+if (process.env.NODE_ENV === 'development') {
+    apiURL = 'http://localhost:8000';
+} else {
+    apiURL = import.meta.env.API_URL;
+}
+
+export const baseURL = apiURL;
